00eebf230e3bd67cd466ffbdf26f797c88519421
ilkeralaiye <ilkeralaiye@hotmail.com>
2025-10-20 13:14:12 +0300

    Code the fundamentals of Neuron class

--- Rent_Estimation_Model/Neuron.cs
+++ Rent_Estimation_Model/Neuron.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rent_Estimation_Model
+﻿namespace Rent_Estimation_Model
 {
-    internal class Neuron
-    {
+    internal class Neuron {
+
+        private double[] weights;
+        public double lambda;
+        Random r;
+
+        public Neuron(int weightCount, double lambda) {
+            this.weights = new double[weightCount];
+            this.lambda = lambda;
+            r = new Random();
+
+            for (int i = 0; i < weightCount; i++) { weights[i] = r.NextDouble() * 2 - 1; }
+        }
+
+        public double[] getWeights() { return this.weights; }
+
+
+
+
     }
 }
