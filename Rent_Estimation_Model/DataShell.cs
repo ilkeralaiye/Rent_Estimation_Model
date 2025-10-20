78fce7c61d8a4ef6a902eff7ed45b14805a0da0f
ilkeralaiye <ilkeralaiye@hotmail.com>
2025-10-20 13:24:28 +0300

    DataShell class coded.

--- Rent_Estimation_Model/DataShell.cs
+++ Rent_Estimation_Model/DataShell.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Rent_Estimation_Model {
+    internal class DataShell {
+        
+        public double[] inputs;
+        public double target;
 
-namespace Rent_Estimation_Model
-{
-    internal class DataShell
-    {
+        /*Divide all Room Count values in Table 1 by 5, 
+         * all Distance from Center values by 20, 
+         * Building Age values to 30, 
+         * and Rent values to 10,000. 
+        */
+        public DataShell(double roomCount, double distanceToCenter, double buildingAge, double rent, bool calculateWithAge) {
+
+            if (calculateWithAge) {
+                inputs = new double[] { roomCount / 5.0, distanceToCenter / 20.0, buildingAge / 30.0 };
+            } else {
+                inputs = new double[] { roomCount / 5.0, distanceToCenter / 20.0 };
+            }
+
+            this.target = rent / 10000.0;
+
+        }
+
     }
 }
