321f5a3761f3ae4e95aeed6f21320fe3cf304c84
ilkeralaiye <ilkeralaiye@hotmail.com>
2025-10-20 13:34:18 +0300

    createDataSets function to read csv files.

--- Rent_Estimation_Model/Program.cs
+++ Rent_Estimation_Model/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Rent_Estimation_Model
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿namespace Rent_Estimation_Model {
+    internal class Program {
+
+        /* Create dataSet which is contains DataShell. Read data from given csv file path. */
+        public static List<DataShell> createDataSets(string path, bool useAgeFeature) {
+
+            var dataSet = new List<DataShell>();
+            var lines = File.ReadAllLines(path);
+            string[] parts;
+
+            for (int i = 0; i < lines.Length; i++) {
+                parts = lines[i].Split(',');
+
+                dataSet.Add(
+                    new DataShell(
+                        int.Parse(parts[0]), // Room Count
+                        double.Parse(parts[1]), // Distance to Center
+                        int.Parse(parts[2]), // Building Age
+                        int.Parse(parts[3]), // Rent
+                        useAgeFeature // Whether to include Building Age as a feature
+                    )
+                );
+
+            }
+
+            return dataSet;
+
+        }
+            static void Main(string[] args) {
+            
         }
     }
 }
