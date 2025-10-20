cf10f3c89c8aa725c7a4a0dbe4262dd2961d1601
ilkeralaiye <ilkeralaiye@hotmail.com>
2025-10-20 13:20:30 +0300

    activationFormula, calculateY and Train functions coded. Neuron.cs is completed

--- Rent_Estimation_Model/Neuron.cs
+++ Rent_Estimation_Model/Neuron.cs
@@ -1,4 +1,6 @@
-﻿namespace Rent_Estimation_Model
+﻿using System.Transactions;
+
+namespace Rent_Estimation_Model
 {
     internal class Neuron {
 
@@ -16,7 +18,24 @@
 
         public double[] getWeights() { return this.weights; }
 
+        public double activationFormula(double v) { return 1.0 / (1.0 + Math.Exp(-v)); }
 
+        public double calculateY(double[] inputs) { 
+            
+            double v = 0.0;
+            for (int i = 0; i < inputs.Length; i++) { v += inputs[i] * weights[i]; }
+
+            return activationFormula(v);
+
+        }
+
+        public void Train(double[]inputs, double target) {
+            double y = calculateY(inputs);
+            double error = target - y;
+
+            for (int i = 0; i < inputs.Length; i++) { weights[i] += this.lambda * error * inputs[i]; }
+
+        }
 
 
     }
